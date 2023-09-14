e065b50241402f92f4e5d771dac936ce089cf393
Rajkumar-2k21 <rajempire2k20@gmail.com>
2023-09-14 11:13:34 +0530

    Update logo and icons

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -49,7 +49,7 @@ function App({ Component, pageProps }: AppProps<{}>) {
               <Navbar
                 setApiKey={setApiKey}
                 onOpen={onOpen}
-                logoText={'Horizon UI Dashboard PRO'}
+                logoText={'NextOne'}
                 brandText={getActiveRoute(routes, pathname)}
                 secondary={getActiveNavbar(routes, pathname)}
               />
